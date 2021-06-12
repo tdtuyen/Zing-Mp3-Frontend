36bebd80c1aa56f0305d1d5c5c8cee3aa970ec97
TuyenTran-C1220G1 <tdtuyen.86@gmail.com>
2021-06-12 10:05:12 +0700

    add detail layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,10 +5,11 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
 import {SharedModule} from '../shared/shared.module';
 import {UserModule} from '../user/user.module';
+import { DetailLayoutComponent } from './detail-layout/detail-layout.component';
 
 
 @NgModule({
-  declarations: [HomeLayoutComponent],
+  declarations: [HomeLayoutComponent, DetailLayoutComponent],
     imports: [
         CommonModule,
         LayoutRoutingModule,
