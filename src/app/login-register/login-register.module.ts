596d97bab943a190fc7be0be593c83bb9645b54c
Le Minh Duc <duclm3.tt>
2021-06-12 19:25:04 +0700

    change display login,register with link

--- src/app/login-register/login-register.module.ts
+++ src/app/login-register/login-register.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { LoginRegisterRoutingModule } from './login-register-routing.module';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [
-    CommonModule,
-    LoginRegisterRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        LoginRegisterRoutingModule,
+        ReactiveFormsModule
+    ],
   exports: [LoginComponent, RegisterComponent]
 })
 export class LoginRegisterModule { }
--- src/app/login-register/register/register.component.ts
+++ src/app/login-register/register/register.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+function comparePassword(c: AbstractControl) {
+  const v = c.value;
+  return (v.password === v.confirmPassword) ? null : {
+    passwordNotMatch: true
+  };
+}
+
+function checkDob(control: AbstractControl) {
+  const from = control.get('dob');
+  const d = new Date();
+  return (new Date(from.value) < d) ? null : {
+    invalidDob: true
+  };
+}
 
 @Component({
   selector: 'app-register',
@@ -6,10 +22,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.min(6), Validators.max(8)]],
+      email: ['', [Validators.required, Validators.email]],
+      pwGroup: this.fb.group({
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+      }, {validator: comparePassword})
+    });
   }
-
+  get f() {
+    return this.registerForm.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      alert('register success!');
+      this.submitted = false;
+    }
+  }
 }
