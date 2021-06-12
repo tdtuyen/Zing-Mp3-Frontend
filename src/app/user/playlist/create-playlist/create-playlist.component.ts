00497abae9cf177ff2337d043fc63213bc67e8e2
nguyenhuyhoanlc <nguyenhuyhoanlc@gmail.com>
2021-06-12 14:23:28 +0700

    done upload avata playlist

--- src/app/user/playlist/create-playlist/create-playlist.component.ts
+++ src/app/user/playlist/create-playlist/create-playlist.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AngularFireStorage, AngularFireStorageReference} from '@angular/fire/storage';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-create-playlist',
@@ -6,13 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-playlist.component.css']
 })
 export class CreatePlaylistComponent implements OnInit {
+  DateOfBirth = new Date();
+  selectedFile: File;
+  ref: AngularFireStorageReference;
+  downloadURL: string;
+  checkUploadAvatar = false;
+  @Output()
+  giveURLtoCreate = new EventEmitter<string>();
+  @Output()
+  sendAvatarUrl = new EventEmitter<string>();
+  constructor(private httClient: HttpClient,
+              private afStorage: AngularFireStorage) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
   }
-
-  date($scope) {
-    $scope.date = new Date();
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+  }
+  onUpload() {
+    this.checkUploadAvatar = true;
+    const id = Math.random().toString(36).substring(2); // upload file tao 1 string random
+    this.ref = this.afStorage.ref(id);
+    this.ref.put(this.selectedFile)
+      .then(snapshot => {
+        return snapshot.ref.getDownloadURL(); // tra ve  1 duong link download
+      })
+      .then(downloadURL => {
+        this.downloadURL = downloadURL;
+        this.giveURLtoCreate.emit(this.downloadURL);
+        console.log(downloadURL);
+        this.checkUploadAvatar = false;
+        return downloadURL;
+      })
+      // tslint:disable-next-line:no-shadowed-variable
+      .catch(error => {
+        console.log(`Failed to upload avatar and get link -${error}`);
+      });
   }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,7 +5,6 @@ import { UserRoutingModule } from './user-routing.module';
 import { FeatureArtitsComponent } from './artist/feature-artits/feature-artits.component';
 import {TopSongComponent} from './song/top-song/top-song.component';
 import {TreandingSongsComponent} from './song/treanding-songs/treanding-songs.component';
-import {ArtistModule} from './artist/artist.module';
 import {NewPlaylistComponent} from './playlist/new-playlist/new-playlist.component';
 import {TopPlaylistComponent} from './playlist/top-playlist/top-playlist.component';
 import {AblumComponent} from './ablum/ablum.component';
